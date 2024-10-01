7077c032cc488b6cd8fc4179d7450370bf07a3d1
Naomi García Robles <naomigarciarobles21@gmail.com>
2024-10-01 13:46:32 +0200

    Create card and array

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import { Card } from "./Card";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const tutorialData = [
+    {
+      title: "Dedica moltes hores",
+      description:
+        "Un mínim de 30 hores a la setmana. Si no en tens prou, haurás de dedicar-li més hores. Al principi sembla impossible, però notaràs una millora ràpidament.",
+      bgColor: "#4db5c6",
+      image: <img src="./images/time_managment.svg" alt="Time" />,
+    },
+    {
+      title: "Programa projectes propis",
+      description:
+        "Més val 10 hores treballant en projectes propis, que 10 hores mirant tutorials. La motivació i la implicació en el projecte ajudarà a accelerar el teu aprenentatge.",
+      bgColor: "#ebebeb",
+      image: <img src="./images/programming.svg" alt="Programing" />,
+    },
+    {
+      title: "Procura descansar",
+      description:
+        "Descansar bé i desconectar són vitals. D'aquesta manera reduiràs l'estrès i l'ansietat. Milloraràs la teva concentració i consolidaràs el teu aprenentatge.",
+      bgColor: "#ffd549",
+      image: <img src="./images/meditation.svg" alt="Meditation" />,
+    },
+  ];
 
-  return (
-    <div className="font-poppins">
-     <button className="btn btn-primary">DaisyUI Button</button>
-    </div>
-  )
+  return <Card />;
 }
-
-export default App
